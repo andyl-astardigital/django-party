c27f050298ec67e50d024e43e694c6b4b61a4969
Andrew Lewis <Andrew.lewis@proactis.com>
2025-02-14 14:55:46 +0000

    add new party and partial validation

--- party/views/__init__.py
+++ party/views/__init__.py
@@ -1,4 +1,15 @@
+
+
+from .new_party_views import page_new_party, partial_check_party_date, partial_check_invitation
+from .party_details_views import PartyDetailPage, PartyDetailPartial
 from .party_list_views import PartyListPage
 
 
-__all__ = ["PartyListPage",]
+__all__ = [
+    "PartyListPage",
+    "PartyDetailPage",
+    "PartyDetailPartial",
+    "page_new_party",
+    "partial_check_party_date",
+    "partial_check_invitation",
+]
